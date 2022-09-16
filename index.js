13c040955d6bc2972513deee23e773d4b9951db5
Lewis Steele <lewis.steele@posteo.net>
2022-09-16 19:47:48 +0100

    🐛 Make it actually work

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-import fg from 'fast-glob';
-import random from 'random';
-import touch from 'touch';
-import { program } from 'commander';
+import fg from 'fast-glob'
+import random from 'random'
+import touch from 'touch'
+import { program } from 'commander'
 
 program
   .name('recursive-touch')
   .argument('date')
   .argument('path')
   .action(async (date, dir) => {
-    const hours = random.int(0, 23);
-    const minutes = random.int(0, 59);
-    const seconds = random.int(0, 59);
+    const hours = random.int(0, 23)
+    const minutes = random.int(0, 59)
+    const seconds = random.int(0, 59)
 
-    const files = [dir, ...(await fg(`${dir}/*`))];
+    const files = [dir, ...(await fg(`${dir}/**/*`))]
     const options = {
       time: `${date} ${hours}:${minutes}:${seconds}`,
-    };
+    }
 
-    files.forEach(async file => await touch(file, options));
-  });
+    files.forEach(async file => await touch(file, options))
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
